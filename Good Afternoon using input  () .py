0625d17031d3aa87ea666021bfaf2be518b43a67
Shyam83-oss <83969699+Shyam83-oss@users.noreply.github.com>
2021-10-12 12:25:36 +0530

    Create Good Afternoon using input  () .py

---
+++ Good Afternoon using input  () .py
@@ -0,0 +1,12 @@
+# Q. Write a python program to display a user entered name followed by Good Afternoon using input() function.
+
+# By - Shyam Mandora
+# Date of Creation - 12-10-2021
+# Last Modified - 12-10-2021
+
+name = input("Enter your name :")
+print("Good Afternoon " + name)
+
+
+# output : Enter your name : Shyam
+#          Good Afternoon Shyam
