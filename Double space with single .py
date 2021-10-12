b3f22f8463c47d61202d513c149a5696a2c48f37
Shyam83-oss <83969699+Shyam83-oss@users.noreply.github.com>
2021-10-12 12:22:24 +0530

    Create Double space with single .py

---
+++ Double space with single .py
@@ -0,0 +1,14 @@
+# Replace double spaces with single spaces in previous program
+
+# By - Shyam Mandora 
+# Date of Creation - 12-10-2021
+# Last Modified - 12-10-2021
+
+string = input("Enter String - ")
+while "  " in string:
+   string=string.replace("  "," ") 
+print( string)
+
+# OUTPUT:-Enter String - Shyam Mandora
+# Shyam Mandora
+
